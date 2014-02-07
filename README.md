4088ef1300cbc57b2a53fcf9cbd93021bfdb8544
Debjit Biswas <debjitbis08@gmail.com>
2014-02-08 00:28:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Ub - Your UIBase Personal Assistant
+===================================
+
+Ub helps you with scaffolding, running tests and creating builds for applications built with
+[UIBase](http://www.uibase.net) framework.
+
+Usage
+-----
+
+```bash
+~$ ub --helps
+
+  Usage: ub [options] [command]
+
+  Commands:
+
+    create:app [options]
+    create:view [options]
+    create:component [options]
+    build:dev [options]
+    build:prod [options]
+    test [options]
+    lint [options]
+
+  Options:
+
+    -h, --help      output usage information
+    -v, --version   output the version number
+```
+
---
+++ cli.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Command = require('commander');
+var pkg = require('./package.json');
+
+var create = require('./lib/create');
+var build = require('./lib/build');
+var lint = require('./lib/lint');
+var test = require('./lib/test');
+
+var cli = new Command();
+
+cli.version(pkg.version);
+
+build.commands.forEach(function(command) {
+    cli.command('build:' + command.name)
+        .description(command.description)
+        .action(command.action);
+});
+
+create.commands.forEach(function(command) {
+    cli.command('create:' + command.name)
+        .description(command.description)
+        .action(command.action);
+});
+
+lint.commands.forEach(function(command) {
+    cli.command(command.name)
+        .description(command.description)
+        .action(command.action);
+});
+
+test.commands.forEach(function(command) {
+    cli.command(command.name)
+        .description(command.description)
+        .action(command.action);
+});
+
